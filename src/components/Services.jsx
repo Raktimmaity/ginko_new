db453e839ee2b8bcc57567686c52568eeb04e61f
Raktimmaity <raktimmaity2003@gmail.com>
2025-05-19 15:12:09 +0530

    update services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -18,24 +18,24 @@ const Services = () => {
           <h2 className="text-4xl font-bold text-yellow-400 text-center mb-10">
             Services <span className="text-white">Offered</span>
           </h2>
-          <div className="bg-gray-800 bg-opacity-80 p-6 rounded-lg shadow-md">
+          <div className="rounded-lg shadow-md">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <ul className="space-y-4 list-none">
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Home Delivery</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Nightlife</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Smoking Area</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Indoor Seating</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Gin Bar</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Takeaway Available</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Serves Cocktails</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Home Delivery</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Nightlife</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Smoking Area</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Indoor Seating</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Gin Bar</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Takeaway Available</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Serves Cocktails</li>
               </ul>
               <ul className="space-y-4 list-none">
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Dance Floor</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Table Reservation Recommended</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Live Music</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Full Bar Available</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Resto Bar</li>
-                <li><Check className="inline text-white bg-green-600 rounded-full p-1"/> Wifi</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Dance Floor</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Table Reservation Recommended</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Live Music</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Full Bar Available</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Resto Bar</li>
+                <li className="bg-gray-800 p-6 rounded-lg"><Check className="inline text-white bg-green-600 rounded-full p-1"/> Wifi</li>
               </ul>
             </div>
           </div>
